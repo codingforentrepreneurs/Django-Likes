8be3ff8ffc5fd3922a8e27da156c093065830707
CFE <hello@teamcfe.com>
2017-03-09 14:20:20 -0800

    Update models

--- src/cfelikes/urls.py
+++ src/cfelikes/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
     url(r'^', include("posts.urls", namespace='posts')),
 ]
 
--- src/posts/models.py
+++ src/posts/models.py
@@ -26,24 +26,24 @@ def upload_location(instance, filename):
     return "%s/%s" %(new_id, filename)
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    title = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True, blank=True)
-    image = models.ImageField(upload_to=upload_location, 
-            null=True, 
-            blank=True, 
-            width_field="width_field", 
-            height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
-    content = models.TextField()
-    draft = models.BooleanField(default=False)
-    publish = models.DateField(auto_now=False, auto_now_add=False)
-    read_time =  models.IntegerField(default=0) # models.TimeField(null=True, blank=True) #assume minutes
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+    title           = models.CharField(max_length=120)
+    slug            = models.SlugField(unique=True, blank=True)
+    image           = models.ImageField(upload_to=upload_location, 
+                        null=True, 
+                        blank=True, 
+                        width_field="width_field", 
+                        height_field="height_field")
+    height_field    = models.IntegerField(default=0)
+    width_field     = models.IntegerField(default=0)
+    content         = models.TextField()
+    draft           = models.BooleanField(default=False)
+    publish         = models.DateField(auto_now=False, auto_now_add=False)
+    read_time       = models.IntegerField(default=0)
+    updated         = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp       = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    objects = PostManager()
+    objects         = PostManager()
 
     def __unicode__(self):
         return self.title
